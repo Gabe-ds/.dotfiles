3fcff44dfa0f97869ee99b7997b46d904c9cdfea
gabe <mahiro.sogabe.2019@ds.musashino-u.ac.jp>
2021-03-15 18:15:16 +0900

    Add create_env.sh

---
+++ create_env.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Install Homebrew
+xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/instal>
+
+# Setup profiles
+git clone https://github.com/Gabe-ds/.dotfiles.git
+cp .dotfiles/.zprofile ./
+cp .dotfiles/.zshrc ./
+source .zprofile .zshrc
+
+# Install pyenv, pipenv
+brew install pyenv pipenv
+
+# Setup Python 3.8.2
+pyenv install 3.8.2
+pyenv global 3.8.2
