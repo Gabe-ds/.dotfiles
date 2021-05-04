96f19e4071b573cc86d6e0767d4d147945f7b04a
gabe <mahiro.sogabe.2019@ds.musashino-u.ac.jp>
2021-05-04 14:27:17 +0900

    add shells. edit create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -13,6 +13,11 @@ source .zprofile .zshrc
 # Install pyenv, pipenv, postgresql
 brew install pyenv pipenv postgresql
 
+# Setting zsh
+brew install zsh
+cp ~/.dotfiles/shells ~/etc/shells
+brew install zsh-completions zsh-autosuggestions zsh-syntax-highlighting
+
 # Setup Python 3.8.2
 pyenv install 3.8.2
 pyenv global 3.8.2
